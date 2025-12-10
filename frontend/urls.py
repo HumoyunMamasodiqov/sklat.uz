c78b01e41daa4931f67233eabf30dbadb4855003
Humoyun Mamasodiqov <mushtariy3748@gmail.com>
2025-12-10 22:19:37 +0500

    sklat.uz Do'kon Boshqaruv Tizimi

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name='base'),
-    path('cart/', views.cart, name='cart')
+    path('home/', views.home, name='home'),
+    path('mahsulotlar/', views.mahsulotlar, name='mahsulotlar'),
+    path('analitika/', views.analitika, name='analitika'),
+    path('mijozlar/', views.mijozlar, name='mijozlar'),
+    path('sozlamalar/', views.sozlamalar, name='sozlamalar'),
 ]   
--- frontend/views.py
+++ frontend/views.py
@@ -5,5 +5,13 @@ def base(request):
     return render(request, 'base.html')
 
 
-def cart(request):
-    return render(request, 'cart.html')
+def home(request):
+    return render(request, 'home.html')
+def mahsulotlar(request):
+    return render(request, 'mahsulotlar.html')
+def analitika(request):
+    return render(request, 'analitika.html')
+def mijozlar(request):
+    return render(request, 'mijozlar.html')
+def sozlamalar(request):
+    return render(request, 'sozlamalar.html')
